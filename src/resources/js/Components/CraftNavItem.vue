c5f6126a1ec717a20a91fa2fb3bad80d5cf2dd97
arthurnassar <arthurnassar@gmail.com>
2023-10-14 18:26:40 +0100

    ci(test): :construction_worker: test new build ci/cd

--- src/resources/js/Components/CraftNavItem.vue
+++ src/resources/js/Components/CraftNavItem.vue
@@ -79,6 +79,7 @@ const root = ref<HTMLElement | null>(null);
 const { width: rootWidth } = useElementSize(root);
 const isCompact = computed(() => rootWidth.value <= 100);
 const open = ref(false);
+//
 </script>
 
 <style scoped lang="scss">
